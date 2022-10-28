d5b9a891ce9ee405284c279be6951cf6c48e97f5
ggoleng2 <115221211+ggoleng2@users.noreply.github.com>
2022-10-28 15:53:28 +0900

    Delete baemin/home directory

--- baemin/home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from order.models import Shop,Menu, Order,Orderfood
-from order.serializers import ShopSerializer, MenuSerializer
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-
-def index(request):
-        return render(request, 'home/index.html',{})
